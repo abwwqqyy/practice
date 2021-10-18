2cae4b055bf98873743009f22b7621bc65d1430a
abwwqqyy <qiyuewang0315@gmail.com>
2021-10-18 00:48:01 -0500

    first

---
+++ src/main/java/QW/q21.java
@@ -0,0 +1,7 @@
+package QW;
+
+public class q21 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/test/java/QW/q21Test.java
@@ -0,0 +1,14 @@
+package QW;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class q21Test {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+
+}
