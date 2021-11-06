c830885f0483072dafe1892f51d67611a2b032ba
abwwqqyy <qiyuewang0315@gmail.com>
2021-11-05 23:04:14 -0500

    11/05/2021

--- src/main/java/QW/LC671MergeTwoBinaryTrees.java
+++ src/main/java/QW/LC671MergeTwoBinaryTrees.java
@@ -3,18 +3,7 @@ package QW;
 import java.util.Deque;
 import java.util.LinkedList;
 
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
- }
+
 
 public class LC671MergeTwoBinaryTrees {
 
