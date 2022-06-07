e600b092b59d0a21817f5ba4917dfd535f2ce1c3
abwwqqyy <qiyuewang0315@gmail.com>
2022-06-07 01:22:25 -0500

    06/06/2022 - 2

---
+++ src/main/java/QW/LC369PlusOneLinkedList.java
@@ -0,0 +1,31 @@
+package QW;
+
+public class LC369PlusOneLinkedList {
+    public ListNode plusOne(ListNode head) {
+        // sentinel head
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode notNine = sentinel;
+
+        // find the rightmost not-nine digit
+        while (head != null) {
+            if (head.val != 9) {
+                notNine = head;
+            }
+            head = head.next;
+        }
+
+        // increase this rightmost not-nine digit by 1
+        notNine.val++;
+        notNine = notNine.next;
+
+        // set all the following nines to zeros
+        while (notNine != null) {
+            notNine.val = 0;
+            notNine = notNine.next;
+        }
+
+        return sentinel.val != 0 ? sentinel : sentinel.next;
+    }
+}
+// time O(n) space O(1)
